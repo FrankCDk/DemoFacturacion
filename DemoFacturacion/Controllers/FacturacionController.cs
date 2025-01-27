b1943f0b56ca9049e22cfe41cf00083ee7740ca0
FrDk <frank28dk@gmail.com>
2025-01-27 00:22:07 -0500

    DEMO

---
+++ DemoFacturacion/Controllers/FacturacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoFacturacion.Controllers
+{
+    public class FacturacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoFacturacion/wwwroot/js/Facturacion.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    console.log("Facturacion.js cargado");
+
+    $('#exampleModal').on('show.bs.modal', function () {
+        // Agrega la clase personalizada al backdrop
+        $('.modal-backdrop').addClass('modal-backdrop-custom');
+    });
+
+    // Evento cuando el modal se oculta
+    $('#exampleModal').on('hidden.bs.modal', function () {
+        // Remueve la clase personalizada al backdrop
+        $('.modal-backdrop').removeClass('modal-backdrop-custom');
+    });
+
+    $(".btn-click").on("click", function () {
+        $('#exampleModal').modal('hide'); // Cierra el modal correctamente
+    });
+
+
+});
